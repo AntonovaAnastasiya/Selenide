c6151c4f8e60831652730223fd4ccdd2d6df4ffd
PS <89146019257@mail.ru>
2022-05-15 23:27:08 +0900

    Update tests

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -3,6 +3,8 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
@@ -12,23 +14,39 @@ import java.time.format.DateTimeFormatter;
 
 
 public class CardDeliveryTest {
-
-    LocalDate today = LocalDate.now();
-    LocalDate newDate = today.plusDays(3);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String meetingDate = newDate.format(formatter);
+    public String generateDate(int planningDate) {
+        return LocalDate.now().plusDays(planningDate).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+}
 
     @Test
-    void shouldRegistredDeliveryOfTheCard(){
+    void shouldRegistredDeliveryOfTheCardIfDateAutomatically(){
+        String planningDate = generateDate(4);
         Configuration.holdBrowserOpen=true;
         open("http://localhost:9999");
         $("[placeholder=\"Город\"]").setValue("Благовещенск");
-        $("[placeholder=\"Дата встречи\"]").setValue(meetingDate);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
         $("[name=\"name\"]").setValue("Ильф Петров");
         $("[name=\"phone\"]").setValue("+79146551203");
         $("[data-test-id=\"agreement\"]").click();
         $x("//*[text()=\"Забронировать\"]").click();
         $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
-        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
+    }
+
+    @Test
+    void shouldRegistredDeliveryOfTheCardIfInputDateManually(){
+        String planningDate = "25.05.2022";
+        Configuration.holdBrowserOpen=true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"]").setValue("Благовещенск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
+        $("[name=\"name\"]").setValue("Ильф Петров");
+        $("[name=\"phone\"]").setValue("+79146551203");
+        $("[data-test-id=\"agreement\"]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
