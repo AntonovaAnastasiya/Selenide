b9d906bf1819f29449d106dad4b4b2202b985539
Анастасия Антонова <96870104+AntonovaAnastasiya@users.noreply.github.com>
2022-05-17 17:06:34 +0900

    Update CardDeliveryTest.java

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -33,20 +33,4 @@ public class CardDeliveryTest {
         $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
         $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
-
-    @Test
-    void shouldRegistredDeliveryOfTheCardIfInputDateManually(){
-        String planningDate = "25.05.2022";
-        Configuration.holdBrowserOpen=true;
-        open("http://localhost:9999");
-        $("[placeholder=\"Город\"]").setValue("Благовещенск");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
-        $("[name=\"name\"]").setValue("Ильф Петров");
-        $("[name=\"phone\"]").setValue("+79146551203");
-        $("[data-test-id=\"agreement\"]").click();
-        $x("//*[text()=\"Забронировать\"]").click();
-        $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
-        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
-    }
 }
